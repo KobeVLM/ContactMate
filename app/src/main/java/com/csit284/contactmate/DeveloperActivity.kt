5f09a974ad31f1ca5cc75166e622181e0060e16e
LAB14_USER ON WS17 <L14X09W17@citulab.net>
2025-03-28 08:25:15 +0800

    Added Developer Activity

---
+++ app/src/main/java/com/csit284/contactmate/DeveloperActivity.kt
@@ -0,0 +1,11 @@
+package com.csit284.contactmate
+
+import android.os.Bundle
+import android.app.Activity
+
+class DeveloperActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.developer)
+    }
+}
